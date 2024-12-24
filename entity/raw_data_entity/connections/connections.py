ceec67902a4c76a703eb05b32d06b34ae7c353d7
Ksenniya <lukkonina1@gmail.com>
2024-12-24 17:05:02 -0300

    saved entity/raw_data_entity/connections/connections.py: 7a505d7e-c231-11ef-98d6-40c2ba0ac9eb

---
+++ entity/raw_data_entity/connections/connections.py
@@ -0,0 +1,4 @@
+{
+    "summary": "Connection details summary: The connection is established with the name 'eSett Open Data API' using HTTP as the connection type. The base URL for the connection is 'https://api.opendata.esett.com', and it includes an endpoint '/EXP01/BalanceResponsibleParties' for retrieving a list of Balance Responsible Parties with options for querying by code, country, and name.",
+    "can_proceed": false
+}

9324a06301a594d7d900e144761eb33d58f83976
app-builder <app-builder@example.com>
2025-02-07 13:35:46 +0000

    Added api registration: 935a2af4-e553-11ef-b476-72938f541e4e

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from common.repository.cyoda.cyoda_init import init_cyoda
 from app_init.app_init import cyoda_token
 #please update this line to your entity
 from entity.payment.api import api_bp_payment
+from entity.customer.api import api_bp_customer
 from entity.ENTITY_NAME_VAR.api import api_bp_ENTITY_NAME_VAR
 
 logging.basicConfig(level=logging.INFO)
@@ -15,6 +16,7 @@ logging.basicConfig(level=logging.INFO)
 app = Quart(__name__)
 QuartSchema(app)
 app.register_blueprint(api_bp_payment, url_prefix='/api/payment')
+app.register_blueprint(api_bp_customer, url_prefix='/api/customer')
 app.register_blueprint(api_bp_ENTITY_NAME_VAR, url_prefix='/api/ENTITY_NAME_VAR')
 
 @app.before_serving

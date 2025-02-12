ba1fe2730af9c247af88a418a4aa26826a3cd788
app-builder <app-builder@example.com>
2025-02-12 15:11:11 +0000

    Added api registration: cecc33d6-e943-11ef-9eca-96210fb9dc23

--- app.py
+++ app.py
@@ -7,12 +7,16 @@ from common.grpc_client.grpc_client import grpc_stream
 from common.repository.cyoda.cyoda_init import init_cyoda
 from app_init.app_init import cyoda_token
 #please update this line to your entity
+from entity.crocodiles.api import api_bp_crocodiles
+from entity.crocodiles_filter.api import api_bp_crocodiles_filter
 from entity.ENTITY_NAME_VAR.api import api_bp_ENTITY_NAME_VAR
 
 logging.basicConfig(level=logging.INFO)
 
 app = Quart(__name__)
 QuartSchema(app)
+app.register_blueprint(api_bp_crocodiles, url_prefix='/api/crocodiles')
+app.register_blueprint(api_bp_crocodiles_filter, url_prefix='/api/crocodiles_filter')
 app.register_blueprint(api_bp_ENTITY_NAME_VAR, url_prefix='/api/ENTITY_NAME_VAR')
 
 @app.before_serving

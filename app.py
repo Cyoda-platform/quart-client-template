baad291b9a33511b500d74530198550a2137c043
app-builder <app-builder@example.com>
2025-02-17 21:17:40 +0000

    Added api registration: c8c45922-ed72-11ef-9b0d-fa2c14a2137e

--- app.py
+++ app.py
@@ -7,12 +7,18 @@ from common.grpc_client.grpc_client import grpc_stream
 from common.repository.cyoda.cyoda_init import init_cyoda
 from app_init.app_init import cyoda_token
 #please update this line to your entity
+from entity.users.api import api_bp_users
+from entity.posts.api import api_bp_posts
+from entity.post.api import api_bp_post
 from entity.ENTITY_NAME_VAR.api import api_bp_ENTITY_NAME_VAR
 
 logging.basicConfig(level=logging.INFO)
 
 app = Quart(__name__)
 QuartSchema(app)
+app.register_blueprint(api_bp_users, url_prefix='/api/users')
+app.register_blueprint(api_bp_posts, url_prefix='/api/posts')
+app.register_blueprint(api_bp_post, url_prefix='/api/post')
 app.register_blueprint(api_bp_ENTITY_NAME_VAR, url_prefix='/api/ENTITY_NAME_VAR')
 
 @app.before_serving

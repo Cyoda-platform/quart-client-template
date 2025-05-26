0cd8d09c3771bdfda5bbd99e6a124e8e455aca20
Ksenniya <lukkonina1@gmail.com>
2025-05-26 10:24:29 -0300

    saved routes/routes.py: a7f84e1e-3e9c-11b2-9252-caa5012250d4

--- routes/routes.py
+++ routes/routes.py
@@ -1,16 +1,7 @@
-from datetime import timezone, datetime
-import logging
-from quart import Blueprint, request, abort
-from quart_schema import validate, validate_querystring, tag, operation_id
-from app_init.app_init import BeanFactory
-from common.service.entity_service_interface import EntityService
+import requests
 
-logger = logging.getLogger(__name__)
-FINAL_STATES = {"FAILURE", "SUCCESS", "CANCELLED", "CANCELLED_BY_USER", "UNKNOWN", "FINISHED"}
-PROCESSING_STATE = "PROCESSING"
-routes_bp = Blueprint("routes", __name__)
+def my_workflow(data):
+    # Sample processing
+    return {'processed': True}
 
-factory = BeanFactory(config={"CHAT_REPOSITORY": "cyoda"})
-entity_service: EntityService = factory.get_services()["entity_service"]
-
-
+service = entity_service(entity_model='example')

6696841b06e9190c19b882e937082d03f8ffccd9
Kseniia <ksenia.lukonina@cyoda.com>
2025-06-23 21:47:36 -0300

    Delete entity/cat/workflow.py

--- entity/cat/workflow.py
+++
@@ -1,7 +0,0 @@
-async def process_start(entity: dict):
-    entity["status"] = "started"
-    entity["workflowProcessed"] = False
-
-async def process_process_cat(entity: dict):
-    entity["cat_processed"] = True
-    entity["workflowProcessed"] = True

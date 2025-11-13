b50ee6da319fb167e37065aa82fa4e5f514fda6d
app-builder <app-builder@example.com>
2025-11-13 23:43:06 +0000

    Generated code using CLI - tasks completed

---
+++ application/entity/pet/__init__.py
@@ -0,0 +1,8 @@
+"""
+Pet entity module for Cyoda Client Application.
+"""
+
+from .version_1 import Pet
+
+__all__ = ["Pet"]
+
---
+++ application/entity/pet/version_1/__init__.py
@@ -0,0 +1,8 @@
+"""
+Pet entity version 1 implementation.
+"""
+
+from .pet import Pet
+
+__all__ = ["Pet"]
+

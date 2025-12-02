64b9312a48fdb39cc2ba54139eb951ed383e54f7
Ksenniya <lukkonina1@gmail.com>
2025-12-02 07:50:23 -0300

    Code generation progress on 132ab678-1a85-458c-be7f-98e5311cb485

--- application/processor/risk_check_processor.py
+++ application/processor/risk_check_processor.py
@@ -12,7 +12,6 @@ from typing import Any, Dict
 from application.entity.order.version_1.order import Order
 from common.entity.entity_casting import cast_entity
 from common.processor.base import CyodaEntity, CyodaProcessor
-from services.services import get_entity_service
 
 
 class RiskCheckProcessor(CyodaProcessor):

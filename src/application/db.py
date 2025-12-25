f6db580c3116dc4d050501466d5e6d87e736a1e5
app-builder <app-builder@example.com>
2025-12-25 13:04:32 +0000

    saved src/application/db.py: 3a1831ec-c1fb-11b2-afc0-0e044684c169

---
+++ src/application/db.py
@@ -0,0 +1,12 @@
+import psycopg2
+
+# Функция для подключения к базе данных
+def create_connection():
+    conn = psycopg2.connect(
+        dbname=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT')
+    )
+    return conn
